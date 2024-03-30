a79a47cb6b46a0ca642931a324e4aa414f6dbb41
권순호 <tnsgh3691@gmail.com>
2024-03-30 21:56:30 +0900

    미완성v3

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ bool gameOver;
 const int width = BOARD_SIZE;
 const int height = BOARD_SIZE;
 int x, y, fruitX, fruitY, score;
+int start;
 int tailX[100], tailY[100];
 int nTail;
 void drawMap();
@@ -51,44 +52,54 @@ void drawMap() {
 
 void Setup() {
   srand(time(nullptr));
-	gameOver = false;;
-	x = width / 2;
+	gameOver = false;
+	x = width / 2; // 뱀의 시작 위치
 	y = height / 2;
-	fruitX = rand() % width +1;
-	fruitY = rand() % height;
+	fruitX = (rand()%(width-2))+1;
+	fruitY = (rand()%(height-2))+1;
 	score = 0;
+	nTail = 0;
+	for(int i=0;i<100;i++)
+	{
+		tailX[i] = -1;
+		tailY[i] = -1;
+	}
 }
 
 void Draw() {
 
-	for (int i = 1; i < height; i++) {
-		for (int j = 0; j < width; j++) {
+	for (int i = 1; i < height-1; i++) 
+	{
+		for (int j = 1; j < width-1; j++) 
+		{
 			if (i == y && j == x)
 				console::draw(j,i,SNAKE_STRING); // 뱀의 머리
 			else if (i == fruitY && j == fruitX)
 				console::draw(j,i,APPLE_STRING); // 사과
-			else {
-				bool print = false;
-				for (int k = 0; k < nTail; k++) {
-					if (tailX[k] == j && tailY[k] == i) {
+			else 
+			{
+				for (int k = 0; k < nTail; k++) 
+				{
+					if (tailX[k] == j && tailY[k] == i)
+					{//tailX[k], tailY[k]의 인덱스에는 뱀의 몸통 좌표가 차례대로 대입되어 있어야함!
 						console::draw(j,i,SNAKE_BODY_STRING); // 뱀의 몸통
-						print = true;
 					}
 				}
-				if (!print)
-					cout << " ";
 			}
 		}
 	}
 
-	cout << "\n";
-	cout << "\n";
-	cout << "Score : " << score;
+	console::draw(5,20,"Score : ");
+	std::string score_str = std::to_string(score);
+	console::draw(13,20,score_str);
+	
 }
 
 
 
-void handleInput() {
+void handleInput() { 
+
+	
   if (console::key(console::K_LEFT)) {
     x--;
   }
@@ -102,7 +113,7 @@ void handleInput() {
     y++;
   }
   if (console::key(console::K_ESC)) {
-    gameOver = true;
+    exit(0);
   }  
   if (console::key(console::K_ENTER)) {
     game();
@@ -113,7 +124,15 @@ void handleInput() {
 
 
 void Logic() {
-	//꼬리
+  //과일을 먹으면 점수를 올리고, 꼬리가 길어지며, 과일을 다시 배치한다.
+	if (x == fruitX && y == fruitY) {
+    srand(time(nullptr));
+		score += 10;
+		fruitX = (rand()%(width-2))+1;
+		fruitY = (rand()%(height-2))+1;
+		nTail++;
+	}
+	//머리자리에 몸통 넣어주는 로직
 	int prevX = tailX[0];
 	int prevY = tailY[0];
 	int prev2X, prev2Y;
@@ -129,21 +148,14 @@ void Logic() {
 	}
 
   //벽을 만나면 게임 종료
-	if (x > width || x < 0 || y > height || y < 0)
+	if (x >= width-1 || x <= 0 || y >= height-1 || y <= 0)
 		gameOver = true;
   //뱀의 꼬리에 머리가 닿으면 게임 종료
-	for (int i = 0; i < nTail; i++) {
+	for (int i = 0; i < nTail; i++) 
+	{
 		if (tailX[i] == x && tailY[i] == y)
 			gameOver = true;
   }
-    //과일을 먹으면 점수를 올리고, 꼬리가 길어지며, 과일을 다시 배치한다.
-	if (x == fruitX && y == fruitY) {
-    srand(time(nullptr));
-		score += 10;
-		fruitX = rand() % width + 1;
-		fruitY = rand() % height;
-		nTail++;
-	}
   
 }
 
@@ -152,15 +164,14 @@ void Logic() {
 
 void game() {
 
-  // 콘솔 라이브러리를 초기화한다.
-  console::init();
-  Setup();
-
+  console::init();// 콘솔 라이브러리를 초기화한다.
+  
+	Setup();
   while (!gameOver) {
     console::clear();
     drawMap();
     Draw();
-    handleInput();
+		handleInput();
     Logic();
     console::wait();
   }
